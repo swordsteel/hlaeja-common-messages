bc4524b77f3e02f33c3fa150ff2929b59a0c1219
Swordsteel <swordsteel@gmail.com>
2025-03-12 00:31:55 +0100

    extract type response to types and add timestamp, and description to type

--- src/main/kotlin/ltd/hlaeja/library/deviceRegistry/Types.kt
+++ src/main/kotlin/ltd/hlaeja/library/deviceRegistry/Types.kt
@@ -3,17 +3,11 @@ package ltd.hlaeja.library.deviceRegistry
 import java.time.ZonedDateTime
 import java.util.UUID
 
-object Type {
-
-    data class Request(
-        val name: String,
-        val description: String,
-    )
+object Types {
 
     data class Response(
         val id: UUID,
-        val timestamp: ZonedDateTime,
         val name: String,
-        val description: String,
+        val timestamp: ZonedDateTime,
     )
 }
